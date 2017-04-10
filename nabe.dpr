c73f5b64abd0c3c525f11a373d24e89b17d1a3f6
fuke masasi <masasi>
2017-04-10 18:46:34 +0900

    すっきりさせました

--- nabe.dpr
+++ nabe.dpr
@@ -13,7 +13,7 @@ begin
   try
     { TODO -oUser -cConsole メイン : ここにコードを記述してください }
     for i := 1 to 40 do
-      if (i mod 3 = 0) or (i mod 10 = 3) or ((i div 10) mod 10 = 3) then
+      if (i mod 3 = 0) or (i mod 10 = 3) or (i div 10 = 3) then
         Writeln('Aho')
       else
         Writeln(i.ToString);
