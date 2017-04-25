bfae1edce2fcd1396f77c1c0432889792cdc7dd8
fuke masasi <yamat0jp@yahoo.co.jp>
2017-04-25 18:44:41 +0900

    階乗を求めるプログラムを追加

---
+++ kaijou.dpr
@@ -0,0 +1,28 @@
+program kaijou;
+
+{$APPTYPE CONSOLE}
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+var
+  s: string;
+  i, j: integer;
+
+begin
+  try
+    { TODO -oUser -cConsole メイン : ここにコードを記述してください }
+    Writeln('階乗を計算します');
+    Readln(s);
+    j := 1;
+    for i := 2 to s.ToInteger do
+      j := j + i;
+    Writeln(j.ToString);
+    Readln;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+
+end.
